78a68dce6d8f6a86e04256e36563ae864bf05604
ivan <imponderabilidad@yandex.ru>
2015-10-03 10:10:36 +0300

    TODO list updated

--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,20 @@ int main(int argc, char *argv[]) {
 /*
  * TODO list:
  *
- *		1. Implement Clear (or "Stop" button).
- *		2. Repair hamming code-decode.
- *		3. Handle 1- and 2-length information messages.
- *		4. Status label should be in the bottom of the layout. Use QStatusBar.
- *		5. Static linking Qt5 on Windows OS.
- *		6. Statistic's layout elements should be fit each other (by width).
- *		7. Try to replace statistics labels with lineEdits with gray background.
- *		8. Cyclic codes widget.
- *		9. Implement multi-threaded process.
- *		10. Icon of application
+ *		1. Repair hamming code-decode.
+ *		2. Handle 1- and 2-length information messages.
+ *		3. Status label should be in the bottom of the layout. Use QStatusBar.
+ *		4. Static linking Qt5 on Windows OS.
+ *		5. Statistic's layout elements should be fit each other (by width).
+ *		6. Try to replace statistics labels with lineEdits with gray background.
+ *		7. Move hamming lab sources into separate folder.
+ *		8. Work with thread should be done in separate class.
+ *		9. Create main widget (for lab selection)
+ *		10. Implement multi-threaded process.
+ *		11. Icon of application
+ *		12. Create cyclic codes lab widget
  *
  *	TODO: out-of-list
- *		1. Decimal delimiter on input fields
- *		2. Highlight check bits in coded message on the screen
+ *		1. Hamming codes. Decimal delimiter on input fields
+ *		2. Hamming codes. Highlight check bits in coded message on the screen
  */
