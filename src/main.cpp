77e6294ef824d23753327db8625a3add23a1d9f5
bobrov.ivan.win <imponderabilidad@yandex.ru>
2015-10-04 19:06:20 +0300

    Updated TODO list.

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,16 @@ int main(int argc, char *argv[]) {
 /*
  * TODO list:
  *
- *		1. Status label should be in the bottom of the layout. Use QStatusBar.
- *		2. Static linking Qt5 on Windows OS.
- *		3. Statistic's layout elements should be fit each other (by width).
- *		4. Try to replace statistics labels with lineEdits with gray background.
- *		5. Move hamming lab sources into separate folder.
- *		6. Work with thread should be done in separate class.
- *		7. Create main widget (for lab selection)
- *		8. Implement multi-threaded process.
- *		9. Icon of application
- *		10. Create cyclic codes lab widget
+ *		1. Static linking Qt5 on Windows OS.
+ *		2. Statistic's layout elements should be fit each other (by width).
+ *		3. Try to replace statistics labels with lineEdits with gray background.
+ *		4. Move hamming lab sources into separate folder.
+ *		5. Work with thread should be done in separate class.
+ *		6. Create main widget (for lab selection)
+ *		7. Implement multi-threaded process.
+ *		8. Icon of application
+ *		9. Create cyclic codes lab widget
+ *		10. Status bar has bigger font size than usual text. It shouldn't be set in points, or set in points everywhere
  *
  *	TODO: out-of-list
  *		1. Hamming codes. Decimal delimiter on input fields
